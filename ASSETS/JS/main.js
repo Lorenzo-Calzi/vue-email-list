36a28cb4a5f270dcea425655aa56c237a4457d38
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-27 15:27:20 +0200

    Aggiunto ciclo for per stampare 10 email

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -2,21 +2,29 @@ const app = new Vue({
     el: '#app',
 
     data: {
-        randomEmail: null
+        randomEmail: null,
+        newArray: []
 
     }, 
     methods: {
 
 
-    }, 
-    mounted() {
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (response => {
-            console.log(response);
-            this.randomEmail = response.data.response
-            console.log(this.randomEmail);
-        })
+    },
 
+    mounted() {
+        for (let index = 0; index < 10; index++) {
+           axios
+           .get('https://flynn.boolean.careers/exercises/api/random/mail')
+           .then(response => {
+                this.randomEmail = response.data.response
+                console.log(this.randomEmail);
+                /* document.writeln(this.randomEmail); */
+                this.newArray.push(this.randomEmail)
+            })
+        }
+        console.log(this.newArray);
+        
     }
-}) 
+})
+
+
