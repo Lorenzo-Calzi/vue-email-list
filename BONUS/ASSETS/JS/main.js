4fa59939a772f9cde0d9a4b84c7a618a8bb23a30
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-28 10:00:27 +0200

    Aggiunto Bonus

---
+++ BONUS/ASSETS/JS/main.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        randomEmail: null,
+        newArray: []
+
+    }, 
+    methods: {
+
+
+    },
+
+    mounted() {
+        for (let index = 0; index < 10; index++) {
+           axios
+           .get('https://flynn.boolean.careers/exercises/api/random/mail')
+           .then(response => {
+                this.randomEmail = response.data.response
+                this.newArray.push(this.randomEmail)
+            }) 
+            .catch(e => {
+                console.error(e);
+            })
+        }
+    }
+})
+
+
